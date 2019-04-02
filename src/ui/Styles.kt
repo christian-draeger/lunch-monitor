879025c8138fa3b75abb70f7423d8add5ec88a70
Christian Dräger <christian-draeger1@gmail.com>
2019-04-02 11:36:54 +0200

    refactor CSS to use nesting

--- src/ui/Styles.kt
+++ src/ui/Styles.kt
@@ -28,31 +28,34 @@ fun styling() =
             minHeight = 200.px
             fontSize = 1.em
             position = RELATIVE
-        }
-        ".meal p" {
-            margin = "10%"
-            fontSize = 1.3.em
-            fontWeight = BOLD
-            textShadow = "1px -1px 0px #ffffff;"
-            color = 0x006400
-        }
-        ".price" {
-            position = ABSOLUTE
-            display = "flex"
-            borderRadius = 50.px
-            color = WHITE
-            fontWeight = BOLD
-            backgroundColor = RED
-            padding = "15px"
-            fontSize = 1.5.em
-            alignItems = CENTER
-            right = 10.px
-            bottom = 10.px
-            height = 20.px
-        }
-        ".restaurant" {
-            color = RED
-            fontSize = 1.3.em
-            marginLeft = 10.percent
+
+            p {
+                margin = "10%"
+                fontSize = 1.3.em
+                fontWeight = BOLD
+                textShadow = "1px -1px 0px #ffffff;"
+                color = 0x006400
+            }
+
+            " .price" {
+                position = ABSOLUTE
+                display = "flex"
+                borderRadius = 50.px
+                color = WHITE
+                fontWeight = BOLD
+                backgroundColor = RED
+                padding = "15px"
+                fontSize = 1.5.em
+                alignItems = CENTER
+                right = 10.px
+                bottom = 10.px
+                height = 20.px
+            }
+
+            " .restaurant" {
+                color = RED
+                fontSize = 1.3.em
+                marginLeft = 10.percent
+            }
         }
     }
