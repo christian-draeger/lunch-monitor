faf5fcf76f76642382b7869eea4776a03dddea91
Christian Dräger <christian-draeger1@gmail.com>
2019-04-02 10:58:55 +0200

    add travis build file

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -6,13 +6,13 @@ import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.withTestApplication
-import org.junit.Test
+import org.junit.jupiter.api.Test
 
 class ApplicationTest {
 
     @Test
-    fun testRoot() {
-        withTestApplication {
+    fun `can render board`() {
+        withTestApplication({ module(testing = true) }) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertThat(response.status()).isEqualTo(HttpStatusCode.OK)
                 response.content?.let {
