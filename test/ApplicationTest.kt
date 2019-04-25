b48c1cd952587527b264dee5456b96455cf4efc4
Christian Dräger <christian-draeger1@gmail.com>
2019-04-25 23:37:32 +0200

    use skrape{it}'s ktor extension

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -6,6 +6,8 @@ import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.withTestApplication
+import it.skrape.ktor.expectHtml
+import it.skrape.matchers.toBe
 import org.junit.jupiter.api.Test
 
 class ApplicationTest {
@@ -15,13 +17,12 @@ class ApplicationTest {
         withTestApplication({ module(testing = true) }) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertThat(response.status()).isEqualTo(HttpStatusCode.OK)
-                response.content?.let {
-                    skrape(it) {
-                        assertThat(title()).isEqualTo("Lunch Monitor")
-                    }
+                response.expectHtml {
+                    title() toBe "Lunch Monitor"
                 }
             }
         }
     }
+
 }
 
